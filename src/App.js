2035af18a96b522482b9810e4b499d6b126a6399
yury-n <yury.nedomolkin@gmail.com>
2019-01-23 01:27:38 +0400

    updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PaletteDropdown from "./PaletteDropdown/PaletteDropdown";
 import Range from "./Range/Range";
 import { Button, Card, Image, Modal, Dropdown } from "semantic-ui-react";
 import MiniCard from "./MiniCard/MiniCard";
+import { ResizableBox } from "react-resizable";
 import "rc-slider/assets/index.css";
 import { numbers } from "./translations";
 import html2canvas from "html2canvas";
@@ -59,93 +60,103 @@ class App extends Component {
                 </Card>
               }
             >
-              <Modal.Content className="modal-content">
-                <div className="modal-sidebar">
-                  <div className="title-container">
-                    <span className="title">Linear Gradient</span>
-                    <br />
-                    <span className="by-author">by</span>
-                    <a className="author" href="twitter.com/user">
-                      Something
-                    </a>
+              <ResizableBox width={640} height={480}>
+                <Modal.Content className="modal-content">
+                  <div className="modal-sidebar">
+                    <div className="title-container">
+                      <span className="title">Linear Gradient</span>
+                      <br />
+                      <span className="by-author">by</span>
+                      <a className="author" href="twitter.com/user">
+                        Something
+                      </a>
+                    </div>
+                    <div className="config-container">
+                      <label className="form-label">Direction</label>
+                      <Dropdown
+                        id="type"
+                        options={[
+                          {
+                            key: 0,
+                            text: "Custom Angle",
+                            value: "custom_angle"
+                          },
+                          { key: 1, text: "Left to Right", value: "right" },
+                          { key: 2, text: "Right to Left", value: "left" },
+                          { key: 3, text: "Top to Bottom", value: "bottom" },
+                          { key: 4, text: "Bottom to Top", value: "top" }
+                        ]}
+                        onChange={(target, { value }) =>
+                          this.setState({ direction: value })
+                        }
+                        defaultValue={direction}
+                        selection
+                      />
+                      {direction === "custom_angle" && (
+                        <>
+                          <label className="form-label">Angle</label>
+                          <Range
+                            min={0}
+                            max={360}
+                            value={angle}
+                            onChange={value => this.setState({ angle: value })}
+                          />
+                        </>
+                      )}
+                      <label className="form-label">Palette</label>
+                      <PaletteDropdown
+                        selectedPalette={palette}
+                        palettes={[
+                          ["#aaf8ba", "#e8e309"],
+                          ["#d39d38", "#4da0b0"],
+                          ["#8E2DE2", "#4A00E0"],
+                          ["#c8f6ff", "#b438d3"]
+                        ]}
+                        onChange={palette => this.setState({ palette })}
+                      />
+                      {palette.map((color, index) => (
+                        <>
+                          <label for="type" className="form-label">
+                            {numbers[index]} color
+                          </label>
+                          <ColorInput
+                            color={color}
+                            onChange={value =>
+                              this.setState({
+                                palette: [
+                                  ...palette.slice(0, index),
+                                  value,
+                                  ...palette.slice(index + 1, palette.length)
+                                ]
+                              })
+                            }
+                            onOpen={() =>
+                              this.setState({ isSelectingColor: true })
+                            }
+                            onClose={() =>
+                              this.setState({ isSelectingColor: false })
+                            }
+                          />
+                        </>
+                      ))}
+                    </div>
                   </div>
-                  <div className="config-container">
-                    <label className="form-label">Direction</label>
-                    <Dropdown
-                      id="type"
-                      options={[
-                        { key: 0, text: "Custom Angle", value: "custom_angle" },
-                        { key: 1, text: "Left to Right", value: "right" },
-                        { key: 2, text: "Right to Left", value: "left" },
-                        { key: 3, text: "Top to Bottom", value: "bottom" },
-                        { key: 4, text: "Bottom to Top", value: "top" }
-                      ]}
-                      onChange={(target, { value }) =>
-                        this.setState({ direction: value })
-                      }
-                      defaultValue={direction}
-                      selection
+                  <div className="modal-preview">
+                    <div
+                      id="capture"
+                      className="preview"
+                      style={styledObject}
                     />
-                    {direction === "custom_angle" && (
-                      <>
-                        <label className="form-label">Angle</label>
-                        <Range
-                          min={0}
-                          max={360}
-                          value={angle}
-                          onChange={value => this.setState({ angle: value })}
-                        />
-                      </>
-                    )}
-                    <label className="form-label">Palette</label>
-                    <PaletteDropdown
-                      selectedPalette={palette}
-                      palettes={[
-                        ["#aaf8ba", "#e8e309"],
-                        ["#d39d38", "#4da0b0"],
-                        ["#8E2DE2", "#4A00E0"],
-                        ["#c8f6ff", "#b438d3"]
-                      ]}
-                      onChange={palette => this.setState({ palette })}
+                    <Button
+                      icon="download"
+                      basic
+                      className="download-button"
+                      content="Download"
+                      onClick={this.download}
                     />
-                    {palette.map((color, index) => (
-                      <>
-                        <label for="type" className="form-label">
-                          {numbers[index]} color
-                        </label>
-                        <ColorInput
-                          color={color}
-                          onChange={value =>
-                            this.setState({
-                              palette: [
-                                ...palette.slice(0, index),
-                                value,
-                                ...palette.slice(index + 1, palette.length)
-                              ]
-                            })
-                          }
-                          onOpen={() =>
-                            this.setState({ isSelectingColor: true })
-                          }
-                          onClose={() =>
-                            this.setState({ isSelectingColor: false })
-                          }
-                        />
-                      </>
-                    ))}
                   </div>
-                </div>
-                <div className="modal-preview">
-                  <div id="capture" className="preview" style={styledObject} />
-                  <Button
-                    icon="download"
-                    basic
-                    className="download-button"
-                    content="Download"
-                    onClick={this.download}
-                  />
-                </div>
-              </Modal.Content>
+                </Modal.Content>
+              </ResizableBox>
             </Modal>
           </Card.Group>
         </div>
