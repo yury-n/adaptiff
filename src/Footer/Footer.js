021aacb226d0e7e72a0ea0410988d530acb06695
yury-n <yury.nedomolkin@gmail.com>
2019-02-28 02:43:00 +0400

    footer restricted width

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,9 +6,11 @@ class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="license">License</div>
-        All images generated on Adaptiff can be used for free. You can use them
-        for commercial and noncommercial purposes.
+        <div className="footer-inner restricted-width-area">
+          <div className="license">License</div>
+          All images generated on Adaptiff can be used for free. You can use
+          them for commercial and noncommercial purposes.
+        </div>
       </footer>
     );
   }
