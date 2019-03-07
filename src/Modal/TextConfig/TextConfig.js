65259c823473a0a396d418fbce5e84fc16f85240
yury-n <yury.nedomolkin@gmail.com>
2019-03-08 01:45:08 +0400

    font styles, range control without inputs

--- src/Modal/TextConfig/TextConfig.js
+++ src/Modal/TextConfig/TextConfig.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 import ColorInput from "../../ColorInput/ColorInput";
 import Range from "../../Range/Range";
 import "./TextConfig.css";
 
 export default props => (
   <div className="text-config-container">
+    <label className="form-label">Font Style</label>
+    <Button.Group basic>
+      <Button className="button-bold">B</Button>
+      <Button className="button-italic active">I</Button>
+      <Button className="button-underline">U</Button>
+    </Button.Group>
     <label className="form-label">Font Size</label>
-    <Range min={6} max={50} value={18} onChange={f => f} />
+    <Range
+      withRangeInputs={false}
+      min={6}
+      max={50}
+      value={18}
+      onChange={f => f}
+    />
+    <label className="form-label">Padding</label>
+    <Range
+      withRangeInputs={false}
+      min={6}
+      max={50}
+      value={18}
+      onChange={f => f}
+    />
     <label className="form-label">Font Color</label>
     <ColorInput color={"#000"} onChange={f => f} />
     <label className="form-label">Background Color</label>
--- src/Range/Range.js
+++ src/Range/Range.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Slider, { createSliderWithTooltip } from 'rc-slider';
+import Slider, { createSliderWithTooltip } from "rc-slider";
 import { Input } from "semantic-ui-react";
 
 import "./Range.css";
@@ -7,30 +7,32 @@ import "./Range.css";
 const SliderWithTooltip = createSliderWithTooltip(Slider);
 
 class Range extends Component {
-  
+  static defaultProps = {
+    withRangeInputs: true
+  };
   state = {
     min: 0,
-    max: 100,
+    max: 100
   };
-  
+
   constructor(props) {
     super(props);
-    this.state  = {
+    this.state = {
       min: props.min,
-      max: props.max,
+      max: props.max
     };
   }
 
-  onMinChange = (e) => {
+  onMinChange = e => {
     this.setState({
-      min: +e.target.value || 0,
+      min: +e.target.value || 0
     });
-  }
-  onMaxChange = (e) => {
+  };
+  onMaxChange = e => {
     this.setState({
-      max: +e.target.value || 100,
+      max: +e.target.value || 100
     });
-  }
+  };
   render() {
     return (
       <>
@@ -39,33 +41,43 @@ class Range extends Component {
           min={this.state.min}
           max={this.state.max}
           className="slider"
-          maximumTrackStyle={{ backgroundColor: 'rgb(216, 215, 215)', height: 3 }}
-          minimumTrackStyle={{ backgroundColor: 'rgb(216, 215, 215)', height: 3 }}
+          maximumTrackStyle={{
+            backgroundColor: "rgb(216, 215, 215)",
+            height: 3
+          }}
+          minimumTrackStyle={{
+            backgroundColor: "rgb(216, 215, 215)",
+            height: 3
+          }}
           handleStyle={{
             height: 20,
             width: 20,
             marginLeft: -10,
             marginTop: -10,
-            backgroundColor: 'white',
-            borderColor: 'rgb(196, 196, 196)',
-            borderWidth: '1px',
-            boxShadow: '0 3px 4px 0px #d4d4d5c9'
+            backgroundColor: "white",
+            borderColor: "rgb(196, 196, 196)",
+            borderWidth: "1px",
+            boxShadow: "0 3px 4px 0px #d4d4d5c9"
           }}
           onChange={this.props.onChange}
-          tipProps={{ placement: 'bottom', overlayClassName: 'slider-tooltip' }}
+          tipProps={{ placement: "bottom", overlayClassName: "slider-tooltip" }}
         />
-        <div className="range">
-        <Input
-        disabled={this.props.disabledInputs}
-        className="range-input"
-          value={this.state.min} onChange={this.onMinChange}
-        />
-        <Input
-        disabled={this.props.disabledInputs}
-        className="range-input"
-        value={this.state.max} onChange={this.onMaxChange}
-        />
-        </div>
+        {this.props.withRangeInputs && (
+          <div className="range-inputs">
+            <Input
+              disabled={this.props.disabledRangeInputs}
+              className="range-input"
+              value={this.state.min}
+              onChange={this.onMinChange}
+            />
+            <Input
+              disabled={this.props.disabledRangeInputs}
+              className="range-input"
+              value={this.state.max}
+              onChange={this.onMaxChange}
+            />
+          </div>
+        )}
       </>
     );
   }
