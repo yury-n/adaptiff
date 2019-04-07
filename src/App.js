905b95385f6f81915a50ed3000da88e5986477a6
yury-n <yury.nedomolkin@gmail.com>
2019-04-08 01:17:03 +0400

    remove debugging prop

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
             <MiniCard {...bubbles} />
             <MiniCard {...perfectPurple} />
             <MiniCard {...wallPattern} />
-            <MiniCard {...strings25} showModal />
+            <MiniCard {...strings25} />
           </Card.Group>
         </div>
         <Footer />
