69d3d0ba84ae51231f755bd1b0f2a0916e36e58d
yury-n <yury.nedomolkin@gmail.com>
2019-04-14 18:28:14 +0400

    remove debug mode

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
         >
           <Hero />
           <Card.Group className="cards">
-            <MiniCard showModal {...linearGradient} />
+            <MiniCard {...linearGradient} />
             <MiniCard {...trianglify} />
             <MiniCard {...particles} />
             <MiniCard {...bubbles} />
