f89b7a18dffc1a4ee2c2288c7216798b3740c572
yury-n <yury.nedomolkin@gmail.com>
2019-04-25 02:04:48 +0400

    contacts

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -22,19 +22,44 @@ class Header extends Component {
             </div>
           </a>
           <div className={s["header-buttons"]}>
-            <Button icon className={s["social-button"]}>
+            <Button
+              icon
+              className={s["social-button"]}
+              target="_blank"
+              href="https://www.instagram.com/adaptiff"
+            >
               <Icon name="instagram" />
             </Button>
-            <Button icon className={s["social-button"]}>
+            <Button
+              icon
+              className={s["social-button"]}
+              target="_blank"
+              href="https://www.twitter.com/adaptiff_com"
+            >
               <Icon name="twitter" />
             </Button>
-            <Button icon className={s["social-button"]}>
+            <Button
+              icon
+              className={s["social-button"]}
+              target="_blank"
+              href="https://t.me/adaptiff"
+            >
               <Icon name="telegram plane" />
             </Button>
-            <Button icon className={s["contact-button"]}>
+            <Button
+              icon
+              className={s["contact-button"]}
+              target="_blank"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSddoo8_28sRF5Pt7AZae5wtTXPH18dNYFoD8kujnf7omcKaDQ/viewform"
+            >
               Contact
             </Button>
-            <Button icon className={s["submit-button"]}>
+            <Button
+              icon
+              className={s["submit-button"]}
+              target="_blank"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSd7mLAVIt6y3J1GRsKZcVfYApD56TiummXnsFFfNTJQ52Rmtg/viewform?usp=sf_link"
+            >
               Submit
             </Button>
           </div>
