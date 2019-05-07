7e8ec10250789ccc4feaab54deef606bc749b213
paqura <slavaavals616@gmail.com>
2019-05-07 15:27:16 +0400

    Update InsertedImage.js

--- src/Modal/InsertedImage/InsertedImage.js
+++ src/Modal/InsertedImage/InsertedImage.js
@@ -6,11 +6,7 @@ import s from "./InsertedImage.module.css";
 
 export default React.memo(
   React.forwardRef(function InsertedImage({ width, height, imageUrl, onResize }, ref) {
-    console.log(width, height);
-
-    const resize = (evt, data) => {
-      onResize(data);
-    };
+    const resize = (evt, data) => onResize(data);
 
     return (
       <ResizableBox width={width} height={height} lockAspectRatio={true} onResize={resize}>
